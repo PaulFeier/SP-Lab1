e1462262dc9f4401300f034793d7a14eccaa1da8
Paul Feier <92182620+PaulFeier@users.noreply.github.com>
2022-10-06 12:31:15 +0300

    Add files via upload

--- src/Book.java
+++ src/Book.java
@@ -1,30 +1,31 @@
 import java.util.*;
 
 public class Book {
-    private String paragraph;
-    private String image;
-    private String table;
+    private List<String> paragraphList = new ArrayList<>();
+    private List<String> imagesList = new ArrayList<>();
+    private List<String> tableList = new ArrayList<>();
     private String title;
-    private List<String> book = new ArrayList<>();
 
     public Book(String title) {
         this.title = title;
     }
 
     public void createNewParagraph(String paragraph) {
-        this.book.add(paragraph);
+        this.paragraphList.add(paragraph);
     }
 
     public void createNewImage(String image) {
-        this.book.add(image);
+        this.imagesList.add(image);
     }
 
     public void createNewTable(String table) {
-        this.book.add(table);
+        this.tableList.add(table);
     }
 
     public void print() {
         System.out.println(this.title);
-        this.book.forEach(System.out::println);
+        this.paragraphList.forEach(System.out::println);
+        this.imagesList.forEach(System.out::println);
+        this.tableList.forEach(System.out::println);
     }
 }
